91a5017344f26e5061a76bb1b92e10535627e9b7
guedesclaudio <cgsarmentosilva@gmail.com>
2022-09-08 16:42:24 -0300

    FEAT: edit move

--- index.js
+++ index.js
@@ -39,6 +39,11 @@ const schemaFlow = joi.object({
     description: joi.string().max(20).required()
 })
 
+const schemaEditFlow = joi.object({
+    money: joi.number(),
+    description: joi.string().max(20)
+})
+
 
 app.post("/signin", async (req, res) => {
 
@@ -265,6 +270,48 @@ app.delete("/deletemove/:id", async (req, res) => {
         res.sendStatus(500)
     }
 
+})
+
+app.put("/putmove/:id", async (req, res) => {
+
+    const {authorization} = req.headers
+    const token = authorization?.replace("Bearer ", "")
+    const {id} = req.params
+    let {money} = req.body
+    const {error} = schemaEditFlow.validate(req.body)
+    let data = req.body
+
+    if(error) {
+        const errors = error.details.map(value => value.message)
+        res.status(422).send(errors)
+        return
+    }
+    if (!token || !id) {
+        res.sendStatus(401)
+        return
+    }
+    if(money) {
+        data = {...data, money: Number(money).toFixed(2)}
+    }
+
+    try {
+        const user = await db.collection("sessions").findOne({token})
+        const move = await db.collection("cashflow").findOne({_id: new ObjectId(id)})
+        
+
+        if(!user || !move) {
+            res.sendStatus(401)
+            return 
+        }
+
+        await db.collection("cashflow").updateOne({_id: new ObjectId(id)}, {$set: 
+            {...data, date: dayjs().format("DD/MM")}
+        })
+        res.sendStatus(200)
+    } catch (error) {
+        res.sendStatus(500)
+        console.error(error)
+    }
 })
 
 app.listen(5000, () => console.log("Server listening on port 5000"))
